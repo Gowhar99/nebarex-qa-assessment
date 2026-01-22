740933f2269cc0a894f58b08e3917b09e5506f8b
Gowhar Nabi <gowharnabimir@gmail.com>
2026-01-22 23:45:42 +0400

    Refine API tests and update gitignore

--- src/test/java/utils/BaseTest.java
+++ src/test/java/utils/BaseTest.java
@@ -1,23 +1,4 @@
-// package utils;
 
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.testng.annotations.*;
-
-// public class BaseTest {
-//     protected WebDriver driver;
-
-//     @BeforeMethod
-//     public void setup() {
-//         driver = new ChromeDriver();
-//         driver.manage().window().maximize();
-//     }
-
-//     @AfterMethod
-//     public void tearDown() {
-//         if (driver != null) driver.quit();
-//     }
-// }
 package utils;
 
 import org.openqa.selenium.WebDriver;
